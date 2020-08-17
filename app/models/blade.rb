c41fa269e0c5424ae08714a5f565eed781876afd
AReallyTallDwarf <aaronverjans1@hotmail.com>
2020-08-17 15:17:31 +0200

    completed associations in models and added after-create for baskets with new register

--- app/models/blade.rb
+++ app/models/blade.rb
@@ -1,2 +1,5 @@
 class Blade < ApplicationRecord
+
+    has_many :basket_blades
+    has_many :catalogue_blades
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :catalogue
+  has_one :basket
+
+  after_create :create_basket
+
+  private
+  def create
+    @basket = Basket.new(current_user)
+    
+    @basket.save  
+  end
+  
 end
