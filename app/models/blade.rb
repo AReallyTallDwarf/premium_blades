33bf562f98f987b44128868cfa8b8cc4d82dccf0
AReallyTallDwarf <aaronverjans1@hotmail.com>
2020-08-18 09:59:13 +0200

    Added the has_one and has_many identifiers in basket, blade, catalogue and user. Added the automatic creation of a basket and catalogue with the registration of a user.

--- app/models/blade.rb
+++ app/models/blade.rb
@@ -1,2 +1,4 @@
 class Blade < ApplicationRecord
+    has_many :basketblades
+    has_many :catalogueblades
 end
--- app/models/catalogue.rb
+++ app/models/catalogue.rb
@@ -1,3 +1,4 @@
 class Catalogue < ApplicationRecord
   belongs_to :user
+  has_many :catalogueblades
 end
